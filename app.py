a5f3fd3d1d2598f16b2c3420310d91a5f7c8a762
Anubhav-Goyal01 <anubhavgoyal101@gmail.com>
2024-07-13 22:56:03 +0530

    Working for multiple YT URLs and Pinecone setup

--- app.py
+++ app.py
@@ -7,14 +7,15 @@ from langchain_openai import AzureChatOpenAI
 from langchain.chains import create_history_aware_retriever, create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
-from utils import get_response_from_query, create_db_from_documents, Generate_Questions, GenerateQuestionsInitial
+from utils import get_response_from_query, create_db_from_documents
+from langchain_pinecone import PineconeVectorStore
 import os
-
+from werkzeug.utils import secure_filename
+import json
 app = Flask(__name__)
 CORS(app)
 
 
-DB_FAISS_PATH = "vectorstore/db_faiss"
 embeddings = AzureOpenAIEmbeddings(
     model= "text-embedding-3-large",
     azure_endpoint = os.environ["AZURE_ENDPOINT"], 
@@ -23,50 +24,58 @@ embeddings = AzureOpenAIEmbeddings(
     azure_deployment=os.environ["AZURE_DEPLOYMENT"],
 )
 
-vectorstore = FAISS.load_local(DB_FAISS_PATH, embeddings, allow_dangerous_deserialization=True)
-retriever = vectorstore.as_retriever()
+def chain_setup():
+    vectorstore = PineconeVectorStore(index_name="test", embedding=embeddings)
+    retriever = vectorstore.as_retriever()
+    llm = AzureChatOpenAI(
+        azure_endpoint = os.environ["AZURE_ENDPOINT"], 
+        api_key=os.environ["API_KEY"],  
+        api_version=os.environ["API_VERSION"],
+        azure_deployment='gpt35t',
+    )
 
-llm = AzureChatOpenAI(
-    azure_endpoint = os.environ["AZURE_ENDPOINT"], 
-    api_key=os.environ["API_KEY"],  
-    api_version=os.environ["API_VERSION"],
-    azure_deployment='gpt35t',
-)
+
+    contextualize_q_system_prompt = """Given a chat history and the latest user question \
+    which might reference context in the chat history, formulate a standalone question \
+    which can be understood without the chat history. Do NOT answer the question, \
+    just reformulate it if needed and otherwise return it as is."""
+    contextualize_q_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", contextualize_q_system_prompt),
+            MessagesPlaceholder("chat_history"),
+            ("human", "{input}"),
+        ]
+    )
+    history_aware_retriever = create_history_aware_retriever(
+        llm, retriever, contextualize_q_prompt
+    )
+
+    qa_system_prompt = """
+        You are a helpful assistant that that can answer questions about youtube videos/ pdf / ppts based on the given transcript as well as uses its own knowledge to provide a more detailed explanation in case the transcript only touches the surface. Please be aware whether the question is about a youtube video, pdf or ppt and answer accordingly.
+        Answer the question based on the given transcript.
+        Transcript: {context}
+                
+        If you feel like you don't have enough information to answer the question, say "I don't know".
+        """
+
+    qa_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", qa_system_prompt),
+            MessagesPlaceholder("chat_history"),
+            ("human", "{input}"),
+        ]
+    )
+    question_answer_chain = create_stuff_documents_chain(llm, qa_prompt)
+    rag_chain = create_retrieval_chain(history_aware_retriever, question_answer_chain)
+    return rag_chain
 
 
-contextualize_q_system_prompt = """Given a chat history and the latest user question \
-which might reference context in the chat history, formulate a standalone question \
-which can be understood without the chat history. Do NOT answer the question, \
-just reformulate it if needed and otherwise return it as is."""
-contextualize_q_prompt = ChatPromptTemplate.from_messages(
-    [
-        ("system", contextualize_q_system_prompt),
-        MessagesPlaceholder("chat_history"),
-        ("human", "{input}"),
-    ]
-)
-history_aware_retriever = create_history_aware_retriever(
-    llm, retriever, contextualize_q_prompt
-)
 
-qa_system_prompt = """
-    You are a helpful assistant that that can answer questions about youtube videos/ pdf / ppts based on the given transcript as well as uses its own knowledge to provide a more detailed explanation in case the transcript only touches the surface. Please be aware whether the question is about a youtube video, pdf or ppt and answer accordingly.
-    Answer the question based on the given transcript.
-    Transcript: {context}
-            
-    If you feel like you don't have enough information to answer the question, say "I don't know".
-    """
+UPLOAD_FOLDER = 'PDFs'
+ALLOWED_EXTENSIONS = {'pdf'}
 
-qa_prompt = ChatPromptTemplate.from_messages(
-    [
-        ("system", qa_system_prompt),
-        MessagesPlaceholder("chat_history"),
-        ("human", "{input}"),
-    ]
-)
-
-question_answer_chain = create_stuff_documents_chain(llm, qa_prompt)
-rag_chain = create_retrieval_chain(history_aware_retriever, question_answer_chain)
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 @app.route('/get_answer', methods=['POST'])
@@ -77,6 +86,7 @@ def get_answer():
         return jsonify({"error": "No question provided"}), 400
 
     try:
+        rag_chain = chain_setup()
         answer_and_questions = get_response_from_query(question, rag_chain)
         return jsonify({
             "Answer": answer_and_questions['Answer'],
@@ -86,23 +96,41 @@ def get_answer():
         return jsonify({"error": str(e)}), 500
 
 
-
 @app.route('/upload', methods=['POST'])
 def upload():
-    data = request.json
-    url = data.get('url')
-    doc_type = data.get('type') 
+    if not os.path.exists(UPLOAD_FOLDER):
+        os.makedirs(UPLOAD_FOLDER)
 
-    url = [url[0], "NLP.pdf"]
-    doc_type = ["youtube", "pdf"]
-    
-    if not url and not doc_type:
-        return jsonify({"error": "Missing URL or type"}), 400
+    # Get files from form-data.
+    file_objects = request.files.getlist("files")
+    uploaded_files = []
+
+    # Save PDF files.
+    for file in file_objects:
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file_path = os.path.join(UPLOAD_FOLDER, filename)
+            file.save(file_path)
+            uploaded_files.append(file_path)
+
+    # Get JSON data from the form-data.
+    json_data = request.form.get('json')
+    if json_data:
+        json_data = json.loads(json_data)
+        urls = json_data.get('documents')
+    else:
+        return jsonify({"error": "Missing documents data"}), 400
+
+    # Combine file paths and URLs.
+    all_docs = uploaded_files + urls
+    all_doc_types = ['pdf'] * len(uploaded_files) + ['yt'] * len(urls)
+
+    print(all_docs)
+    print(all_doc_types)
 
     try:
-        response_message = create_db_from_documents(url, doc_type)
-        questions = GenerateQuestionsInitial(rag_chain)
-        return jsonify({"questions": [ques for ques in questions]})
+        response_message = create_db_from_documents(all_docs, all_doc_types)
+        return jsonify({"response": "Embeddings Generated Successfully"}), 200
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
--- utils.py
+++ utils.py
@@ -3,7 +3,7 @@ from langchain_community.document_loaders import PyPDFLoader
 from langchain_community.document_loaders import UnstructuredPowerPointLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_openai import AzureOpenAIEmbeddings
-from langchain_community.vectorstores import FAISS
+from langchain_pinecone import PineconeVectorStore
 from dotenv import load_dotenv
 from langchain_core.messages import HumanMessage
 import openai
@@ -11,8 +11,6 @@ import os
 load_dotenv()
 
 chat_history = []
-DB_FAISS_PATH = "vectorstore/db_faiss"
-
 embeddings = AzureOpenAIEmbeddings(
     model= "text-embedding-3-large",
     azure_endpoint = os.environ["AZURE_ENDPOINT"], 
@@ -24,13 +22,13 @@ embeddings = AzureOpenAIEmbeddings(
 
 def Generate_Questions(question, answer):
     client = openai.AzureOpenAI(
-        azure_endpoint = os.environ["AZURE_ENDPOINT"], 
-        api_key=os.environ["API_KEY"],  
-        api_version=os.environ["API_VERSION"],
+        azure_endpoint = os.environ["AZURE_ENDPOINT_GPT_4"], 
+        api_key=os.environ["API_KEY_GPT_4"],  
+        api_version=os.environ["API_VERSION_GPT_4"],
     )
 
     completion = client.chat.completions.create(
-        model='gpt35t',
+        model='sfslackbot',
         messages=[
             {"role": "system", "content": f"""User will provide you with a question and a answer. Based on the pair, generate 3 most probable follow up questions the user might ask. Give the result in the form a list of questions. 
             Example Output: ["Question1", "Question2", "Quesiton3"]. MAKE SURE YOU GIVE PYTHON LIST AS A OUTPUT.
@@ -59,7 +57,9 @@ def create_db_from_documents(documents: list, doc_types: list):
     for document, doc_type in zip(documents, doc_types):
         if doc_type == "pdf":
             loader = PyPDFLoader(document)
-        elif doc_type == "youtube":
+        elif doc_type == "yt":
+            if document == "":
+                continue
             loader = YoutubeLoader.from_youtube_url(document, add_video_info=False)
         elif doc_type == "ppt":
             loader = UnstructuredPowerPointLoader(document)
@@ -72,9 +72,11 @@ def create_db_from_documents(documents: list, doc_types: list):
         all_docs.extend(docs)
         print(f"Loaded {len(docs)} documents from {doc_type}.")     
 
-
-    vectorstore = FAISS.from_documents(documents= all_docs, embedding=embeddings)
-    vectorstore.save_local(DB_FAISS_PATH)
+    vectorstore_from_docs = PineconeVectorStore.from_documents(
+        all_docs,
+        index_name="test",
+        embedding=embeddings
+    )
     return "Vector store created successfully."
 
 
